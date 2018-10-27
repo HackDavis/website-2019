9aecdae72349caf7547459eac39331ff9f945b3a
Cyrus Goh <hello@lovincyrus.com>
2018-10-26 23:17:49 -0700

    Add typed

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+  strings: ["750+ hackers", "24 hours", "$8k prizes", "100 projects demoed"],
+  typeSpeed: 25,
+  backSpeed: 5,
+  fadeOut: true,
+  loop: true,
+  shuffle: false,
+  cursorChar: '_'
+});
