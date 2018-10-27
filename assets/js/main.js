420fcd06e570f1801e1880a76518ce559a623f2a
Cyrus Goh <hello@lovincyrus.com>
2018-10-27 16:48:08 -0700

    Update typed speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 var typed = new Typed('#typed', {
   strings: ["// code for social good", "February 9 - 10, 2019", "750+ hackers", "24 hours", "$8k prizes", "100 projects demoed"],
   typeSpeed: 25,
-  backSpeed: 5,
+  backSpeed: 8,
   backDelay: 1500,
-  startDelay: 200,
-  fadeOut: true,
+  startDelay: 1000,
+  fadeOut: false,
   loop: true,
   shuffle: false,
   cursorChar: '_'
