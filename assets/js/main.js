011e50f92889e2926fc64cff3e584f9431777116
Cyrus Goh <hello@lovincyrus.com>
2018-10-27 14:47:47 -0700

    Optimize splash

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 var typed = new Typed('#typed', {
-  strings: ["750+ hackers", "24 hours", "$8k prizes", "100 projects demoed"],
+  strings: ["// code for social good", "February 9 - 10, 2019", "750+ hackers", "24 hours", "$8k prizes", "100 projects demoed"],
   typeSpeed: 25,
   backSpeed: 5,
+  backDelay: 1500,
+  startDelay: 200,
   fadeOut: true,
   loop: true,
   shuffle: false,
