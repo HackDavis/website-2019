2e1d713529b6312819dfb894c1db637d8b9794ae
Cyrus Goh <hello@lovincyrus.com>
2018-10-28 14:00:09 -0700

    Add scroll to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,3 +85,18 @@ $(document).ready(function(){
     });
   }
 });
+
+// scroll to top 
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {    // If page is scrolled more than 50px
+      $('#top').fadeIn("fast");       // Fade in the arrow
+  } else {
+      $('#top').fadeOut("fast");      // Else fade out the arrow
+  }
+});
+
+$('#top').click(function() {            // When arrow is clicked
+  $('body,html').animate({
+      scrollTop : 0                   // Scroll to top of body
+  }, 500);
+});
