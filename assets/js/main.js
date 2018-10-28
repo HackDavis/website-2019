5048a50fdb176ad5d79d91388506e7d973038a61
Cyrus Goh <hello@lovincyrus.com>
2018-10-28 00:54:31 -0700

    Add progress indicator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// set typed
 var typed = new Typed('#typed', {
   strings: ["// code for social good", "February 9 - 10, 2019", "750+ hackers", "24 hours", "$8k prizes", "100 projects demoed"],
   typeSpeed: 25,
@@ -8,4 +9,67 @@ var typed = new Typed('#typed', {
   loop: true,
   shuffle: false,
   cursorChar: '_'
+});
+
+// get attributes
+$(document).ready(function(){
+  var getMax = function(){
+    return $(document).height() - $(window).height();
+  }
+  
+  var getValue = function(){
+      return $(window).scrollTop();
+  }
+  
+  if('max' in document.createElement('progress')){
+      // Browser supports progress element
+      var progressBar = $('progress');
+      
+      // Set the Max attr for the first time
+      progressBar.attr({ max: getMax() });
+
+      $(document).on('scroll', function(){
+        // On scroll only Value attr needs to be calculated
+        progressBar.attr({ value: getValue() });
+      });
+    
+      $(window).resize(function(){
+        // On resize, both Max/Value attr needs to be calculated
+        progressBar.attr({ max: getMax(), value: getValue() });
+      });   
+  } else {
+      var progressBar = $('.progress-bar'), 
+        max = getMax(), 
+        value, width;
+      
+      var getWidth = function(){
+        // Calculate width in percentage
+        value = getValue();            
+        width = (value/max) * 100;
+        width = width + '%';
+        return width;
+      }
+      
+      var setWidth = function(){
+        progressBar.css({ width: getWidth() });
+      }
+      
+      $(document).on('scroll', setWidth);
+      $(window).on('resize', function(){
+        // Need to reset the Max attr
+        max = getMax();
+        setWidth();
+      });
+  }
+});
+
+// init progress indicator
+$(document).ready(function(){
+  $('#progressBar').addClass('flat');
+
+  $(document).on('scroll', function(){
+      maxAttr = $('#progressBar').attr('max');
+      valueAttr = $('#progressBar').attr('value');
+      percentage = (valueAttr/maxAttr) * 100;  
+  });
 });
